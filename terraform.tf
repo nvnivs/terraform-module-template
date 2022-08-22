ade46369166c5ef814c8dd1598127f4ad1eca4dc
N P <nvnivs@gmail.com>
2022-08-22 09:02:17 +0200

    fix(terraform): Requires TF 0.13 minimum

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {}
-  required_version = ">= 0.12.31, < 2.0.0"
+  required_version = ">= 0.13, < 2.0.0"
   required_providers {
     # Example of requiring the AWS provider
     aws = {
