dcb444e2bf6cc94360c7545e5a3cc2b3308ef665
Nuno Pereira <nvnivs@gmail.com>
2024-05-15 12:04:58 +0200

    Implement tests (#109)
    * rebase branch (#108)
    
    * chore(deps): update pre-commit hook antonbabenko/pre-commit-terraform to v1.89.1
    
    * chore(deps): update tflint plugin terraform-linters/tflint-ruleset-terraform to v0.7.0 (#107)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * feat(test): Adds tests
    
    * feat(devcontainer): Adds go extension
    
    * feat(test): Adds terratest
    
    * fix(tet): Fixes identation
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

---
+++ tests/name_output_test.go
@@ -0,0 +1,23 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNameOutputRequiresInput(t *testing.T) {
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../",
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	output := terraform.Output(t, terraformOptions, "name")
+
+	assert.Equal(t, "value", output)
+}
